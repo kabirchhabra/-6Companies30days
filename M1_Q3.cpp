fd35fdb4dd0db33da8755a596a33a94217495287
Kabir Chhabra <54521120+kabirchhabra@users.noreply.github.com>
2022-01-05 11:06:08 +0530

    Milestone 1

---
+++ M1_Q3.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        int i=0,j=0;
+        long long prod=1;
+        int sum=0;
+        while(j<n)
+        {
+            prod*=a[j];
+            while(prod>=k && j>=i)
+            {
+                prod/=a[i];
+                i++;
+            }
+            sum+=j-i+1;
+            j++;
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
---
+++ M1_Q4.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*You are required to complete this function */
+
+string encode(string src)
+{     
+  //Your code here 
+  int n= src.size();
+  string ans;
+  
+  int i=0;
+  while(i<n){
+      int count=1;
+      while(src[i]==src[i+1]){
+          count++;
+          i++;
+      }
+      ans=ans+src[i]+to_string(count);
+      i++;
+  }
+  return ans;
+}     
+ 
